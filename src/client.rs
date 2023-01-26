c2548d6244b34d8e643a39d478590c36a1b78046
Fisstouk <lyronn.levy@gmail.com>
2023-01-26 02:08:57 +0100

    Added loops on server/client, error with type in server

--- src/client.rs
+++ src/client.rs
@@ -4,7 +4,9 @@ use std::net::TcpStream;
 pub fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:5000").unwrap();
 
-    stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
-    Ok(())
+    loop {
+        stream.write(&[1])?;
+        stream.read(&mut [0; 128])?;
+        Ok(())
+    }
 } // the stream is closed here
--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,16 @@
-use std::net::TcpListener;
+use std::net::{SocketAddr, TcpListener};
 
-pub fn main ()
-{
-let listener = TcpListener::bind("127.0.0.1:5000").expect("Socket not bound to local host");
+pub fn main() {
 
-match listener.accept() 
-    {
-        Ok((_socket, addr)) => println!("New client: {addr:?}"),
-        Err(e) => println!("Couldn't get client: {e:?}"),
-    }
+    let listener = TcpListener::bind("127.0.0.1:5000").expect("Socket not bound to local host");
 
+    let mut server_socket: Vec<SocketAddr> = loop {
+        match listener.accept() {
+            Ok((_socket, addr)) => {return;}, // Break early and return addr
+            Err(e) => println!("Couldn't get client: {e:?}"),
+        };
+    };
+    assert_eq!("127.0.0.1:5000".parse().unwrap(), server_socket);
+    // Keep socket thus keep connection with client
+    server_socket.push(server_socket);
 }
