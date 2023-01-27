33c393a2c990132c55dc6433da8f59a18b03c7a2
Fisstouk <lyronn.levy@gmail.com>
2023-01-27 02:45:57 +0100

    Corrected errors, warning remains

--- src/client.rs
+++ src/client.rs
@@ -2,11 +2,11 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 pub fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:5000").unwrap();
+    let mut stream = TcpStream::connect("127.0.0.1:6000").expect("Cannot connect to server");
 
     loop {
         stream.write(&[1])?;
         stream.read(&mut [0; 128])?;
-        Ok(())
+        // Ok(())
     }
 } // the stream is closed here
--- src/server.rs
+++ src/server.rs
@@ -2,15 +2,17 @@ use std::net::{SocketAddr, TcpListener};
 
 pub fn main() {
 
-    let listener = TcpListener::bind("127.0.0.1:5000").expect("Socket not bound to local host");
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Socket not bound to local host");
 
-    let mut server_socket: Vec<SocketAddr> = loop {
+    let mut server_socket: SocketAddr = loop {
         match listener.accept() {
             Ok((_socket, addr)) => {return;}, // Break early and return addr
             Err(e) => println!("Couldn't get client: {e:?}"),
         };
     };
-    assert_eq!("127.0.0.1:5000".parse().unwrap(), server_socket);
+    // assert_eq!("127.0.0.1:8080".parse().unwrap(), server_socket);
+    
     // Keep socket thus keep connection with client
-    server_socket.push(server_socket);
+    let mut save_server: Vec<SocketAddr> = Vec::new();
+    save_server.push(server_socket);
 }
